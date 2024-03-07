7a88277b03541cda0f5c85e3f5578b21f80d9150
Joseph Lweya <84285814+lweyajoe@users.noreply.github.com>
2024-03-07 16:17:41 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Rendering the App component to the root element in the HTML
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
